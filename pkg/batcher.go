a1d8bb15dd73f8f8c102163abecec1674d2291c7
Hagar Meir <Hagar.Meir@ibm.com>
2024-05-27 14:59:06 +0300

    move logger and add idea to gitignore (#96)
    Signed-off-by: Hagar Meir <hagar.meir@ibm.com>

--- pkg/batcher.go
+++ pkg/batcher.go
@@ -11,14 +11,6 @@ import (
 	"time"
 )
 
-type Logger interface {
-	Debugf(template string, args ...interface{})
-	Infof(template string, args ...interface{})
-	Errorf(template string, args ...interface{})
-	Warnf(template string, args ...interface{})
-	Panicf(template string, args ...interface{})
-}
-
 type RequestInspector interface {
 	RequestID(req []byte) string
 }
