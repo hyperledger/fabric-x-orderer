529146137b7abc0cb476933c898f0b16283b8ba3
YACOV MANEVICH <Yacov.Manevich@ibm.com>
2024-06-03 10:25:26 +0200

    Add all fields of BAF to ToBeSignedBAF() (#100)
    Signed-off-by: Yacov Manevich <yacov.manevich@ibm.com>

--- node/consensus.go
+++ node/consensus.go
@@ -291,7 +291,7 @@ func ToBeSignedComplaint(c *arma.Complaint) []byte {
 }
 
 func ToBeSignedBAF(baf arma.BatchAttestationFragment) []byte {
-	buff := make([]byte, 2+8+2+32)
+	buff := make([]byte, 2+8+2+8+2+32+len(baf.GarbageCollect())*32)
 	var pos int
 	binary.BigEndian.PutUint16(buff, uint16(baf.Shard()))
 	pos += 2
@@ -299,7 +299,16 @@ func ToBeSignedBAF(baf arma.BatchAttestationFragment) []byte {
 	pos += 8
 	binary.BigEndian.PutUint16(buff[pos:], uint16(baf.Signer()))
 	pos += 2
+	binary.BigEndian.PutUint64(buff[pos:], baf.Epoch())
+	pos += 8
+	binary.BigEndian.PutUint16(buff[pos:], uint16(baf.Primary()))
+	pos += 2
 	copy(buff[pos:], baf.Digest())
+	pos += 32
+	for _, gc := range baf.GarbageCollect() {
+		copy(buff[pos:], gc)
+		pos += 32
+	}
 
 	return buff
 }
