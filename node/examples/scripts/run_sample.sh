5416b7773627a65d23e77f8da7cc893b26f39d3f
Natalie Morad <Natalie.Morad@ibm.com>
2024-07-28 13:47:56 +0300

    Implement volume mounts in compose.yaml, add README for scripts (#127) (#182)
    Signed-off-by: Natalie Morad <natalie.morad@ibm.com>

--- node/examples/scripts/run_sample.sh
+++ node/examples/scripts/run_sample.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -eux
 
-docker-compose up -d
+cd ../cmd/armageddon/main && go build -o armageddon
+./armageddon generate --config="../../../examples/config/example-deployment.yaml"
 
+cd ../../../examples && docker-compose up -d  
 sleep 10
 
-docker run -it --entrypoint "/usr/local/bin/armageddon" --network examples_default arma submit --config arma-config/Party1/user_config.yaml --transactions 1000 --rate 500
-
+docker volume create --name arma-config-vol --opt type=none --opt device=$(pwd)/../cmd/armageddon/main/arma-config --opt o=bind
+docker run -it --mount source=arma-config-vol,target=/arma-config --entrypoint /usr/local/bin/armageddon --network examples_default arma submit --config /arma-config/Party1/user_config.yaml --transactions 1000 --rate 500
