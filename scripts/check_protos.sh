9b293de2cc920a11b35221482d6fe82a6fe8d05e
Natalie Morad <Natalie.Morad@ibm.com>
2024-10-01 19:27:38 +0300

    CI refactor (#300)
    Signed-off-by: Natalie Morad <natalie.morad@ibm.com>

---
+++ scripts/check_protos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+output=$(make protos 2>&1)
+if [ $? -ne 0 ]; then 
+    echo "$output"
+    exit 1 
+fi 
+
+git diff --quiet --exit-code -- '*.pb.go' || {
+    echo "The following .pb.go files have been modified after compilation:"
+    git diff --name-only -- '*.pb.go'
+    exit 1
+}
