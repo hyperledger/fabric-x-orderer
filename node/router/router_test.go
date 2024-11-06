2a989915794f6549d44ad8817a24dcf5cd3aad25
May Buzaglo <May.Buzaglo@ibm.com>
2024-11-06 12:45:37 +0200

    Test router against stub batcher (#320)
    * stub batcher
    
    Signed-off-by: May.Buzaglo <May.Buzaglo@ibm.com>
    
    * test router against stub batcher
    
    Signed-off-by: May.Buzaglo <May.Buzaglo@ibm.com>
    
    * address review comments
    
    Signed-off-by: May.Buzaglo <May.Buzaglo@ibm.com>
    
    ---------
    
    Signed-off-by: May.Buzaglo <May.Buzaglo@ibm.com>

--- node/router/router_test.go
+++ node/router/router_test.go
@@ -1,4 +1,4 @@
-package router
+package router_test
 
 import (
 	"context"
@@ -6,11 +6,15 @@ import (
 	"fmt"
 	"io"
 	"sync"
-	"sync/atomic"
 	"testing"
 	"time"
 
-	"arma/common/types"
+	"arma/node/router"
+
+	"arma/node/config"
+
+	"google.golang.org/grpc/grpclog"
+
 	"arma/node/comm"
 	"arma/node/comm/tlsgen"
 	"arma/testutil"
@@ -21,84 +25,160 @@ import (
 	"google.golang.org/grpc"
 )
 
-type testBatcher struct {
-	ops     uint32
-	address string
+func init() {
+	// set the gRPC logger to a logger that discards the log output.
+	grpclog.SetLoggerV2(grpclog.NewLoggerV2(io.Discard, io.Discard, io.Discard))
 }
 
-func newTestBatcher(ca tlsgen.CA, t *testing.T) *testBatcher {
-	ckp, err := ca.NewServerCertKeyPair("127.0.0.1")
+type routerTestSetup struct {
+	ca         tlsgen.CA
+	router     *comm.GRPCServer
+	batcher    *stubBatcher
+	clientConn *grpc.ClientConn
+}
+
+func createRouterTestSetup(t *testing.T) *routerTestSetup {
+	// create a CA that issues a certificate for the router and the batcher
+	ca, err := tlsgen.NewCA()
 	require.NoError(t, err)
 
-	srv, err := comm.NewGRPCServer("127.0.0.1:0", comm.ServerConfig{
-		SecOpts: comm.SecureOptions{
-			UseTLS:      true,
-			Certificate: ckp.Cert,
-			Key:         ckp.Key,
-		},
-	})
+	// create a stub batcher
+	batcher := NewStubBatcher(t, ca)
+
+	// start the batcher
+	batcher.Start()
+
+	// create and start router
+	routerServer := createAndStartRouter(t, ca, batcher.server.Address())
+
+	// create a client to the router
+	conn := createClientConnToRouter(t, ca, routerServer)
+
+	return &routerTestSetup{
+		ca:         ca,
+		router:     routerServer,
+		batcher:    &batcher,
+		clientConn: conn,
+	}
+}
+
+// Scenario:
+// 1. start a client, router and stub batcher
+// 2. send 10 requests by client to router
+// 3. check that the batcher received the expected number of requests
+func TestStubBatcherReceivesClientRouterRequests(t *testing.T) {
+	routerTestSetup := createRouterTestSetup(t)
+
+	err := submitStreamRequests(routerTestSetup.clientConn, 10)
+	require.NoError(t, err)
+
+	require.Eventually(t, func() bool {
+		return routerTestSetup.batcher.ReceivedMessageCount() == uint32(10)
+	}, 10*time.Second, 10*time.Millisecond)
+}
+
+// Scenario:
+// 1. start a client, router and stub batcher
+// 2. send a request by client to router
+// 3. check that the batcher received one request
+func TestStubBatcherReceivesClientRouterSingleRequest(t *testing.T) {
+	routerTestSetup := createRouterTestSetup(t)
+
+	err := submitRequest(routerTestSetup.clientConn)
+	require.NoError(t, err)
+
+	require.Eventually(t, func() bool {
+		return routerTestSetup.batcher.ReceivedMessageCount() == uint32(1)
+	}, 10*time.Second, 10*time.Millisecond)
+}
+
+func TestClientRouterFailsToSendRequestOnBatcherServerStop(t *testing.T) {
+	routerTestSetup := createRouterTestSetup(t)
+
+	// send request, should succeed
+	err := submitRequest(routerTestSetup.clientConn)
+	require.NoError(t, err)
+	require.Eventually(t, func() bool {
+		return routerTestSetup.batcher.ReceivedMessageCount() == uint32(1)
+	}, 10*time.Second, 10*time.Millisecond)
+
+	// stop the batcher and send request, expect to error
+	routerTestSetup.batcher.Stop()
+	err = submitRequest(routerTestSetup.clientConn)
+	require.NotNil(t, err)
+	require.EqualError(t, err, "could not establish stream to "+routerTestSetup.batcher.server.Address())
+}
+
+func submitStreamRequests(conn *grpc.ClientConn, numOfRequests int) error {
+	cl := protos.NewRequestTransmitClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	stream, err := cl.SubmitStream(ctx)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
-	tb := &testBatcher{address: srv.Address()}
-
-	protos.RegisterRequestTransmitServer(srv.Server(), tb)
-	go func() {
-		if err := srv.Start(); err != nil {
-			panic(err)
-		}
-	}()
-
-	return tb
-}
-
-func (t *testBatcher) Submit(ctx context.Context, request *protos.Request) (*protos.SubmitResponse, error) {
-	panic("implement me")
-}
-
-func (t *testBatcher) SubmitStream(stream protos.RequestTransmit_SubmitStreamServer) error {
-	responses := make(chan *protos.SubmitResponse, 1000)
-
-	quit := make(chan struct{})
-
-	defer close(quit)
+	var wg sync.WaitGroup
+	wg.Add(1)
 
 	go func() {
-		for {
-			select {
-			case <-quit:
+		defer wg.Done()
+		buff := make([]byte, 300)
+		for j := 0; j < numOfRequests; j++ {
+			binary.BigEndian.PutUint32(buff, uint32(j))
+			err := stream.Send(&protos.Request{Payload: buff})
+			if err != nil {
 				return
-			case resp := <-responses:
-				stream.Send(resp)
 			}
 		}
 	}()
 
-	for {
-		msg, err := stream.Recv()
-		if err == io.EOF {
-			return nil
-		}
-		if err != nil {
-			return err
-		}
-
-		atomic.AddUint32(&t.ops, 1)
-
-		responses <- &protos.SubmitResponse{
-			TraceId: msg.TraceId,
+	for j := 0; j < numOfRequests; j++ {
+		select {
+		default:
+			resp, err := stream.Recv()
+			if err != nil {
+				return fmt.Errorf("error receiving response: %w", err)
+			}
+			if resp.Error != "" {
+				return fmt.Errorf("receiving response with error: %s", resp.Error)
+			}
+		case <-ctx.Done():
+			return fmt.Errorf("a time out occured during submitting request: %w", ctx.Err())
 		}
 	}
+
+	wg.Wait()
+
+	return nil
 }
 
-func TestRouter(t *testing.T) {
-	t.Skip()
-	ca, err := tlsgen.NewCA()
-	require.NoError(t, err)
+func submitRequest(conn *grpc.ClientConn) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
 
-	testBatcher := newTestBatcher(ca, t)
+	buff := make([]byte, 300)
+	binary.BigEndian.PutUint32(buff, uint32(12345))
+	req := &protos.Request{
+		Payload: buff,
+	}
 
+	cl := protos.NewRequestTransmitClient(conn)
+	resp, err := cl.Submit(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	if resp.Error != "" {
+		return fmt.Errorf(resp.Error)
+	}
+
+	return nil
+}
+
+func createAndStartRouter(t *testing.T, ca tlsgen.CA, batcherEndpoint string) *comm.GRPCServer {
 	ckp, err := ca.NewServerCertKeyPair("127.0.0.1")
 	require.NoError(t, err)
 
@@ -112,13 +192,24 @@ func TestRouter(t *testing.T) {
 			Key:         ckp.Key,
 		},
 	})
+	require.NoError(t, err)
 
-	l := testutil.CreateLogger(t, 0)
+	logger := testutil.CreateLogger(t, 0)
 
-	router := createRouter([]types.ShardID{1}, []string{testBatcher.address}, map[types.ShardID][][]byte{1: {ca.CertBytes()}}, ckp.Cert, ckp.Key, l, 0, 0)
+	shards := []config.ShardInfo{{ShardId: 1, Batchers: []config.BatcherInfo{{PartyID: 1, Endpoint: batcherEndpoint, TLSCACerts: []config.RawBytes{ca.CertBytes()}}}}}
+	config := config.RouterNodeConfig{
+		PartyID:                       1,
+		TLSCertificateFile:            ckp.Cert,
+		TLSPrivateKeyFile:             ckp.Key,
+		ListenAddress:                 srv.Address(),
+		Shards:                        shards,
+		NumOfConnectionsForBatcher:    0,
+		NumOfgRPCStreamsPerConnection: 0,
+	}
+	router := router.NewRouter(config, logger)
 
+	// start a router
 	protos.RegisterRequestTransmitServer(srv.Server(), router)
-
 	go func() {
 		err := srv.Start()
 		if err != nil {
@@ -126,77 +217,20 @@ func TestRouter(t *testing.T) {
 		}
 	}()
 
-	time.Sleep(time.Second)
-
-	connections := 50
-	workerPerConn := 50
-	workPerWorker := 5000
-
-	var wg sync.WaitGroup
-	wg.Add(workerPerConn * connections)
-
-	t1 := time.Now()
-	for i := 0; i < connections; i++ {
-		i := i
-		go func() {
-			cc := comm.ClientConfig{
-				SecOpts: comm.SecureOptions{
-					UseTLS:        true,
-					ServerRootCAs: [][]byte{ca.CertBytes()},
-				},
-				DialTimeout:  time.Second,
-				AsyncConnect: false,
-			}
-
-			time.Sleep(time.Millisecond * 50 * time.Duration(i))
-			conn, err := cc.Dial(srv.Address())
-			require.NoError(t, err)
-
-			for k := 0; k < workerPerConn; k++ {
-				go invokeStream(&wg, conn, workPerWorker)
-			}
-		}()
-	}
-
-	wg.Wait()
-
-	opsPerformed := int(atomic.LoadUint32(&testBatcher.ops))
-	elapsed := time.Since(t1)
-
-	fmt.Println("total seconds:", elapsed, "total operations:", opsPerformed, "TPS:", opsPerformed/int(elapsed.Seconds()))
+	return srv
 }
 
-// func invokeRPC(wg *sync.WaitGroup, conn *grpc.ClientConn, workPerWorker int, txn []byte) {
-// 	defer wg.Done()
-// 	cl := protos.NewRequestTransmitClient(conn)
-// 	for j := 0; j < workPerWorker; j++ {
-// 		cl.Submit(context.Background(), &protos.Request{Payload: txn})
-// 	}
-// }
-
-func invokeStream(wg *sync.WaitGroup, conn *grpc.ClientConn, workPerWorker int) {
-	defer wg.Done()
-	cl := protos.NewRequestTransmitClient(conn)
-	stream, err := cl.SubmitStream(context.Background())
-	if err != nil {
-		panic(err)
+func createClientConnToRouter(t *testing.T, ca tlsgen.CA, routerServer *comm.GRPCServer) *grpc.ClientConn {
+	cc := comm.ClientConfig{
+		SecOpts: comm.SecureOptions{
+			UseTLS:        true,
+			ServerRootCAs: [][]byte{ca.CertBytes()},
+		},
+		DialTimeout:  time.Second,
+		AsyncConnect: false,
 	}
 
-	go func() {
-		buff := make([]byte, 300)
-		for j := 0; j < workPerWorker; j++ {
-			binary.BigEndian.PutUint32(buff, uint32(j))
-			stream.Send(&protos.Request{Payload: buff})
-		}
-	}()
-
-	for j := 0; j < workPerWorker; j++ {
-		resp, err := stream.Recv()
-		if err != nil {
-			panic(err)
-		}
-		if resp.Error != "" {
-			panic(resp.Error)
-		}
-	}
+	conn, err := cc.Dial(routerServer.Address())
+	require.NoError(t, err)
+	return conn
 }
---
+++ node/router/stub_batcher_test.go
@@ -0,0 +1,95 @@
+package router_test
+
+import (
+	"context"
+	"io"
+	"sync/atomic"
+	"testing"
+
+	"arma/node/comm"
+	"arma/node/comm/tlsgen"
+	protos "arma/node/protos/comm"
+
+	"github.com/stretchr/testify/require"
+)
+
+type stubBatcher struct {
+	ca     tlsgen.CA        // Certificate authority that issues a certificate for the batcher
+	server *comm.GRPCServer // GRPCServer instance represents the batcher
+	txs    uint32           // Number of txs received from router
+}
+
+func NewStubBatcher(t *testing.T, ca tlsgen.CA) stubBatcher {
+	// create a (cert,key) pair for the batcher
+	certKeyPair, err := ca.NewServerCertKeyPair("127.0.0.1")
+	require.NoError(t, err)
+
+	// create a GRPC Server which will listen for incoming connections on some available port
+	server, err := comm.NewGRPCServer("127.0.0.1:0", comm.ServerConfig{
+		SecOpts: comm.SecureOptions{
+			UseTLS:      true,
+			Certificate: certKeyPair.Cert,
+			Key:         certKeyPair.Key,
+		},
+	})
+	require.NoError(t, err)
+
+	// return a stub batcher that includes all server setup
+	stubBatcher := stubBatcher{
+		ca:     ca,
+		server: server,
+	}
+	return stubBatcher
+}
+
+func (sb *stubBatcher) Start() {
+	protos.RegisterRequestTransmitServer(sb.server.Server(), sb)
+	go func() {
+		if err := sb.server.Start(); err != nil {
+			panic(err)
+		}
+	}()
+}
+
+func (sb *stubBatcher) Stop() {
+	sb.server.Stop()
+}
+
+func (sb *stubBatcher) Submit(ctx context.Context, request *protos.Request) (*protos.SubmitResponse, error) {
+	resp := &protos.SubmitResponse{
+		Error:   "",
+		ReqID:   request.Identity,
+		TraceId: request.TraceId,
+	}
+	atomic.AddUint32(&sb.txs, 1)
+	return resp, nil
+}
+
+func (sb *stubBatcher) SubmitStream(stream protos.RequestTransmit_SubmitStreamServer) error {
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		resp := &protos.SubmitResponse{
+			Error:   "",
+			ReqID:   req.Identity,
+			TraceId: req.TraceId,
+		}
+
+		atomic.AddUint32(&sb.txs, 1)
+
+		err = stream.Send(resp)
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func (sb *stubBatcher) ReceivedMessageCount() uint32 {
+	return atomic.LoadUint32(&sb.txs)
+}
