3bfb21804a3f2c4b5fb2d6c6ade408442a5a5088
Genady Gurevich <GENADYG@il.ibm.com>
2025-03-20 09:05:39 +0200

    added broadcast tests (#640)

--- node/router/router_test.go
+++ node/router/router_test.go
@@ -11,6 +11,8 @@ import (
 
 	"google.golang.org/grpc/grpclog"
 
+	"github.com/hyperledger/fabric-protos-go-apiv2/common"
+	ab "github.com/hyperledger/fabric-protos-go-apiv2/orderer"
 	"github.ibm.com/decentralized-trust-research/arma/common/types"
 	"github.ibm.com/decentralized-trust-research/arma/node/comm"
 	"github.ibm.com/decentralized-trust-research/arma/node/comm/tlsgen"
@@ -29,15 +31,16 @@ func init() {
 }
 
 type routerTestSetup struct {
-	ca         tlsgen.CA
-	router     *comm.GRPCServer
-	batchers   []*stubBatcher
-	clientConn *grpc.ClientConn
+	ca           tlsgen.CA
+	routerServer *comm.GRPCServer
+	batchers     []*stubBatcher
+	clientConn   *grpc.ClientConn
+	router       *router.Router
 }
 
 func (r *routerTestSetup) Close() {
 	r.clientConn.Close()
-	r.router.Stop()
+	r.routerServer.Stop()
 
 	for _, batcher := range r.batchers {
 		batcher.server.Stop()
@@ -62,23 +65,25 @@ func createRouterTestSetup(t *testing.T, partyID types.PartyID, numOfShards int)
 	}
 
 	// create and start router
-	routerServer := createAndStartRouter(t, partyID, ca, batchers)
+	routerServer, router := createAndStartRouter(t, partyID, ca, batchers)
 
 	// create a client to the router
 	conn := createClientConnToRouter(t, ca, routerServer)
 
 	return &routerTestSetup{
-		ca:         ca,
-		router:     routerServer,
-		batchers:   batchers,
-		clientConn: conn,
+		ca:           ca,
+		routerServer: routerServer,
+		batchers:     batchers,
+		clientConn:   conn,
+		router:       router,
 	}
 }
 
 // Scenario:
 // 1. start a client, router and stub batcher
-// 2. send 10 requests by client to router
-// 3. check that the batcher received the expected number of requests
+// 2. submit 10 requests by client to router
+// 3. brodcast 10 requests by client to router
+// 4. check that the batcher received the expected number of requests
 func TestStubBatcherReceivesClientRouterRequests(t *testing.T) {
 	testSetup := createRouterTestSetup(t, types.PartyID(1), 1)
 	defer testSetup.Close()
@@ -86,15 +91,19 @@ func TestStubBatcherReceivesClientRouterRequests(t *testing.T) {
 	err := submitStreamRequests(testSetup.clientConn, 10)
 	require.NoError(t, err)
 
+	err = submitBroadcastRequests(testSetup.clientConn, 10)
+	require.NoError(t, err)
+
 	require.Eventually(t, func() bool {
-		return testSetup.batchers[0].ReceivedMessageCount() == uint32(10)
+		return testSetup.batchers[0].ReceivedMessageCount() == uint32(20)
 	}, 10*time.Second, 10*time.Millisecond)
 }
 
 // Scenario:
 // 1. start a client, router and stub batcher
-// 2. send a request by client to router
-// 3. check that the batcher received one request
+// 2. submit a request by client to router
+// 3. brodcast a request by client to router
+// 4. check that the batcher received one request
 func TestStubBatcherReceivesClientRouterSingleRequest(t *testing.T) {
 	testSetup := createRouterTestSetup(t, types.PartyID(1), 1)
 	defer testSetup.Close()
@@ -102,8 +111,11 @@ func TestStubBatcherReceivesClientRouterSingleRequest(t *testing.T) {
 	err := submitRequest(testSetup.clientConn)
 	require.NoError(t, err)
 
+	err = submitBroadcastRequests(testSetup.clientConn, 1)
+	require.NoError(t, err)
+
 	require.Eventually(t, func() bool {
-		return testSetup.batchers[0].ReceivedMessageCount() == uint32(1)
+		return testSetup.batchers[0].ReceivedMessageCount() == uint32(2)
 	}, 10*time.Second, 10*time.Millisecond)
 }
 
@@ -168,6 +180,27 @@ func TestClientRouterSubmitStreamRequestsAgainstMultipleBatchers(t *testing.T) {
 	}, 60*time.Second, 10*time.Millisecond)
 }
 
+func TestClientRouterBroadcastRequestsAgainstMultipleBatchers(t *testing.T) {
+	numOfShards := 2
+	testSetup := createRouterTestSetup(t, types.PartyID(1), numOfShards)
+	defer testSetup.Close()
+
+	err := submitBroadcastRequests(testSetup.clientConn, 10)
+	require.NoError(t, err)
+
+	recvCond := func() uint32 {
+		receivedTxCount := uint32(0)
+		for i := 0; i < numOfShards; i++ {
+			receivedTxCount += testSetup.batchers[i].ReceivedMessageCount()
+		}
+		return receivedTxCount
+	}
+
+	require.Eventually(t, func() bool {
+		return recvCond() == uint32(10)
+	}, 60*time.Second, 10*time.Millisecond)
+}
+
 func submitStreamRequests(conn *grpc.ClientConn, numOfRequests int) error {
 	cl := protos.NewRequestTransmitClient(conn)
 
@@ -214,6 +247,52 @@ func submitStreamRequests(conn *grpc.ClientConn, numOfRequests int) error {
 	return nil
 }
 
+func submitBroadcastRequests(conn *grpc.ClientConn, numOfRequests int) error {
+	cl := ab.NewAtomicBroadcastClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	stream, err := cl.Broadcast(ctx)
+	if err != nil {
+		return err
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		buff := make([]byte, 300)
+		for j := 0; j < numOfRequests; j++ {
+			binary.BigEndian.PutUint32(buff, uint32(j))
+			err := stream.Send(&common.Envelope{Payload: buff})
+			if err != nil {
+				return
+			}
+		}
+	}()
+
+	for j := 0; j < numOfRequests; j++ {
+		select {
+		default:
+			resp, err := stream.Recv()
+			if err != nil {
+				return fmt.Errorf("error receiving response: %w", err)
+			}
+			if resp.Status != 200 {
+				return fmt.Errorf("receiving response with error: %s", resp.Status)
+			}
+		case <-ctx.Done():
+			return fmt.Errorf("a time out occured during submitting request: %w", ctx.Err())
+		}
+	}
+
+	wg.Wait()
+
+	return nil
+}
+
 func submitRequest(conn *grpc.ClientConn) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 	defer cancel()
@@ -237,7 +316,7 @@ func submitRequest(conn *grpc.ClientConn) error {
 	return nil
 }
 
-func createAndStartRouter(t *testing.T, partyID types.PartyID, ca tlsgen.CA, batchers []*stubBatcher) *comm.GRPCServer {
+func createAndStartRouter(t *testing.T, partyID types.PartyID, ca tlsgen.CA, batchers []*stubBatcher) (*comm.GRPCServer, *router.Router) {
 	ckp, err := ca.NewServerCertKeyPair("127.0.0.1")
 	require.NoError(t, err)
 
@@ -270,8 +349,9 @@ func createAndStartRouter(t *testing.T, partyID types.PartyID, ca tlsgen.CA, bat
 	}
 	router := router.NewRouter(config, logger)
 
-	// start a router
 	protos.RegisterRequestTransmitServer(srv.Server(), router)
+	ab.RegisterAtomicBroadcastServer(srv.Server(), router)
+
 	go func() {
 		err := srv.Start()
 		if err != nil {
@@ -279,7 +359,7 @@ func createAndStartRouter(t *testing.T, partyID types.PartyID, ca tlsgen.CA, bat
 		}
 	}()
 
-	return srv
+	return srv, router
 }
 
 func createClientConnToRouter(t *testing.T, ca tlsgen.CA, routerServer *comm.GRPCServer) *grpc.ClientConn {
