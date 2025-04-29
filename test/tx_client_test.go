730000882d82ab672d71d2993b1a9f6c8dd43e6b
Omar Agbaria <Omar.Agbaria@ibm.com>
2025-04-29 11:34:46 +0300

    test client ready (#700)
    A client for submitting TXs in tests
    
    ---------
    
    Signed-off-by: Omar Agbaria <omar.agbaria@ibm.com>

---
+++ test/tx_client_test.go
@@ -0,0 +1,113 @@
+package test
+
+import (
+	"bytes"
+	"context"
+	"encoding/binary"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/hyperledger/fabric-protos-go-apiv2/common"
+	"github.com/onsi/gomega/gexec"
+	"github.com/stretchr/testify/assert"
+	"github.ibm.com/decentralized-trust-research/arma/cmd/armageddon"
+	"github.ibm.com/decentralized-trust-research/arma/cmd/testutils"
+	"github.ibm.com/decentralized-trust-research/arma/testutil"
+
+	"github.com/stretchr/testify/require"
+
+	"github.ibm.com/decentralized-trust-research/arma/testutil/client"
+)
+
+func TestTxClientSend(t *testing.T) {
+	totalTxNumber := 100
+	dir, err := os.MkdirTemp("", t.Name())
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+	var broadcastClient *client.BroadCastTxClient
+
+	// 1.
+	configPath := filepath.Join(dir, "config.yaml")
+	listeners := testutils.CreateNetwork(t, configPath, 4, 2, "none", "none")
+	require.NoError(t, err)
+	// 2.
+	armageddon := armageddon.NewCLI()
+	armageddon.Run([]string{"generate", "--config", configPath, "--output", dir, "--version", "2"})
+
+	// 3.
+	// compile arma
+	armaBinaryPath, err := gexec.BuildWithEnvironment("github.ibm.com/decentralized-trust-research/arma/cmd/arma/main", []string{"GOPRIVATE=github.ibm.com"})
+	require.NoError(t, err)
+	require.NotNil(t, armaBinaryPath)
+
+	// run arma nodes
+	// NOTE: if one of the nodes is not started within 10 seconds, there is no point in continuing the test, so fail it
+	readyChan := make(chan struct{}, 20)
+	armaNetwork := testutils.RunArmaNodes(t, dir, armaBinaryPath, readyChan, listeners)
+	defer armaNetwork.Stop()
+
+	startTimeout := time.After(30 * time.Second)
+	for i := 0; i < 20; i++ {
+		select {
+		case <-readyChan:
+		case <-startTimeout:
+			require.Fail(t, "arma nodes failed to start in time")
+		}
+	}
+	// 4. Send To Routers
+	uc, err := testutil.GetUserConfig(dir, 1)
+	assert.NoError(t, err)
+	assert.NotNil(t, uc)
+	broadcastClient = client.NewBroadCastTxClient(uc, 10*time.Second)
+	require.NoError(t, err)
+	for i := 0; i < totalTxNumber; i++ {
+		txContent := prepareTx(i, 100, []byte("sessionNumber"))
+		err = broadcastClient.SendTx(txContent)
+		require.NoError(t, err)
+	}
+	// 5. Check If Transaction is sent
+	t.Log("Finished submit")
+
+	// Pull some block from the middle and count them
+	startBlock := uint64(0)
+	endBlock := uint64(5)
+	totalTxs := 0
+	totalBlocks := 0
+
+	dc := client.NewDeliverClient(uc)
+	cnx, cancel := context.WithCancel(context.Background())
+	handler := func(block *common.Block) error {
+		totalTxs += len(block.Data.Data)
+		totalBlocks++
+		if totalTxs == totalTxNumber+1 {
+			cancel()
+			return context.Canceled
+		}
+		return nil
+	}
+	dc.PullBlocks(cnx, 1, startBlock, endBlock, handler)
+	defer broadcastClient.Stop()
+	assert.Equal(t, totalTxNumber+1, totalTxs)
+
+	t.Logf("Finished pull and count: %d, %d", totalBlocks, totalTxs)
+}
+
+func prepareTx(txNumber int, txSize int, sessionNumber []byte) []byte {
+	// create timestamp (8 bytes)
+	timeStamp := uint64(time.Now().UnixNano())
+
+	// prepare the payload
+	buffer := make([]byte, txSize)
+	buff := bytes.NewBuffer(buffer[:0])
+	buff.Write(sessionNumber)
+	binary.Write(buff, binary.BigEndian, uint64(txNumber))
+	binary.Write(buff, binary.BigEndian, timeStamp)
+	result := buff.Bytes()
+	if len(buff.Bytes()) < txSize {
+		padding := make([]byte, txSize-len(result))
+		result = append(result, padding...)
+	}
+	return result
+}
---
+++ testutil/client/tx_broadcast_client.go
@@ -0,0 +1,94 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/hyperledger/fabric-protos-go-apiv2/common"
+	ab "github.com/hyperledger/fabric-protos-go-apiv2/orderer"
+	"github.ibm.com/decentralized-trust-research/arma/cmd/armageddon"
+	"github.ibm.com/decentralized-trust-research/arma/node/comm"
+)
+
+type BroadCastTxClient struct {
+	userConfig       *armageddon.UserConfig
+	timeOut          time.Duration
+	streamRoutersMap map[string]ab.AtomicBroadcast_BroadcastClient
+}
+
+func NewBroadCastTxClient(userConfigFile *armageddon.UserConfig, timeOut time.Duration) *BroadCastTxClient {
+	return &BroadCastTxClient{
+		userConfig:       userConfigFile,
+		timeOut:          timeOut,
+		streamRoutersMap: make(map[string]ab.AtomicBroadcast_BroadcastClient),
+	}
+}
+
+func (c *BroadCastTxClient) SendTx(txContent []byte) error {
+	c.createSendStreams()
+	failures := 0
+	var errorsAccumulator strings.Builder
+	for key, stream := range c.streamRoutersMap {
+		err := stream.Send(&common.Envelope{Payload: txContent})
+		if err != nil {
+			delete(c.streamRoutersMap, key)
+			errorsAccumulator.WriteString(err.Error())
+			failures++
+		}
+	}
+	if failures >= len(c.userConfig.RouterEndpoints)/3 {
+		er := fmt.Sprintf("\nfailed to send tx to %d out of %d send streams", failures, len(c.streamRoutersMap))
+		errorsAccumulator.WriteString(er)
+	}
+	if errorsAccumulator.Len() > 0 {
+		return fmt.Errorf("%v", errorsAccumulator.String())
+	}
+	return nil
+}
+
+func (c *BroadCastTxClient) createSendStreams() error {
+	userConfig := c.userConfig
+	serverRootCAs := append([][]byte{}, userConfig.TLSCACerts...)
+
+	// create gRPC clients and streams to the routers
+	for i := 0; i < len(userConfig.RouterEndpoints); i++ {
+		_, ok := c.streamRoutersMap[userConfig.RouterEndpoints[i]]
+		if !ok {
+			// create a gRPC connection to the router
+			gRPCRouterClient := comm.ClientConfig{
+				KaOpts: comm.KeepaliveOptions{
+					ClientInterval: time.Hour,
+					ClientTimeout:  time.Hour,
+				},
+				SecOpts: comm.SecureOptions{
+					Key:               userConfig.TLSPrivateKey,
+					Certificate:       userConfig.TLSCertificate,
+					RequireClientCert: userConfig.UseTLSRouter == "mTLS",
+					UseTLS:            false,
+					ServerRootCAs:     serverRootCAs,
+				},
+				DialTimeout: time.Second * 5,
+			}
+			gRPCRouterClientConn, err := gRPCRouterClient.Dial(userConfig.RouterEndpoints[i])
+			if err == nil {
+				stream, err := ab.NewAtomicBroadcastClient(gRPCRouterClientConn).Broadcast(context.TODO())
+				if err == nil {
+					c.streamRoutersMap[userConfig.RouterEndpoints[i]] = stream
+				} else {
+					gRPCRouterClientConn.Close()
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func (c *BroadCastTxClient) Stop() {
+	for key := range c.streamRoutersMap {
+		if stream, ok := c.streamRoutersMap[key]; ok {
+			stream.CloseSend()
+		}
+	}
+}
