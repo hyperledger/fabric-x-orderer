c84f007750d54b1e09393a66bcc36396522e8915
Natalie Morad <Natalie.Morad@ibm.com>
2025-05-07 15:49:03 +0300

    control event broadcaster unit test (#723)
    * control event broadcaster unit test
    
    Signed-off-by: Natalie Morad <natalie.morad@ibm.com>
    
    * after review
    
    Signed-off-by: Natalie Morad <natalie.morad@ibm.com>
    
    * fix
    
    Signed-off-by: Natalie Morad <natalie.morad@ibm.com>
    
    ---------
    
    Signed-off-by: Natalie Morad <natalie.morad@ibm.com>

---
+++ node/batcher/control_event_broadcaster_test.go
@@ -0,0 +1,96 @@
+package batcher_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.ibm.com/decentralized-trust-research/arma/core"
+	"github.ibm.com/decentralized-trust-research/arma/node/batcher"
+	"github.ibm.com/decentralized-trust-research/arma/node/batcher/mocks"
+	"github.ibm.com/decentralized-trust-research/arma/testutil"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBroadcastControlEventQuorumScenarios(t *testing.T) {
+	type testCase struct {
+		name        string
+		f           int
+		n           int
+		succ        int
+		fail        int
+		expectError bool
+	}
+
+	tests := []testCase{
+		{name: "n=4 f=1 all succeed", n: 4, f: 1, succ: 4, fail: 0, expectError: false},
+		{name: "n=4 f=1 one fails", n: 4, f: 1, succ: 3, fail: 1, expectError: false},
+		{name: "n=4 f=1 two fail (no quorum)", n: 4, f: 1, succ: 2, fail: 2, expectError: true},
+
+		{name: "n=7 f=2 all succeed", n: 7, f: 2, succ: 7, fail: 0, expectError: false},
+		{name: "n=7 f=2 two fail", n: 7, f: 2, succ: 5, fail: 2, expectError: false},
+		{name: "n=7 f=2 three fail (no quorum)", n: 7, f: 2, succ: 4, fail: 3, expectError: true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			logger := testutil.CreateLogger(t, 1)
+
+			var senders []batcher.ConsenterControlEventSender
+			for i := 0; i < tt.succ; i++ {
+				s := &mocks.FakeConsenterControlEventSender{}
+				s.SendControlEventReturns(nil)
+				senders = append(senders, s)
+			}
+
+			for i := 0; i < tt.fail; i++ {
+				s := &mocks.FakeConsenterControlEventSender{}
+				s.SendControlEventReturns(errors.New(""))
+				senders = append(senders, s)
+			}
+
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			b := batcher.NewControlEventBroadcaster(senders, tt.n, tt.f, 10*time.Millisecond, 100*time.Millisecond, logger, ctx, cancel)
+			defer b.Stop()
+
+			err := b.BroadcastControlEvent(core.ControlEvent{})
+			if tt.expectError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestControlEventBroadcasterSenderRetry(t *testing.T) {
+	logger := testutil.CreateLogger(t, 1)
+	var senders []batcher.ConsenterControlEventSender
+
+	// two senders succeed
+	for i := 0; i < 2; i++ {
+		s := &mocks.FakeConsenterControlEventSender{}
+		s.SendControlEventReturns(nil)
+		senders = append(senders, s)
+	}
+
+	// sender fails once, then succeeds
+	sRetry := &mocks.FakeConsenterControlEventSender{}
+	sRetry.SendControlEventReturnsOnCall(1, errors.New(""))
+	sRetry.SendControlEventReturnsOnCall(2, nil)
+	senders = append(senders, sRetry)
+
+	// sender always fail
+	sFail := &mocks.FakeConsenterControlEventSender{}
+	sFail.SendControlEventReturns(errors.New(""))
+	senders = append(senders, sFail)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	b := batcher.NewControlEventBroadcaster(senders, 4, 1, 10*time.Millisecond, 100*time.Millisecond, logger, ctx, cancel)
+	defer b.Stop()
+
+	err := b.BroadcastControlEvent(core.ControlEvent{})
+	require.NoError(t, err)
+}
