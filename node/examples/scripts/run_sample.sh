cbd6ff40459fb644ed3fe25df59bb769f8cf1c43
Yoav Tock <tock@il.ibm.com>
2025-07-07 11:08:52 +0300

    Initial CI pt. 4 (#6)
    Sample tests on a dockerized deployment
    
    Signed-off-by: Yoav Tock <tock@il.ibm.com>

--- node/examples/scripts/run_sample.sh
+++ node/examples/scripts/run_sample.sh
@@ -6,10 +6,11 @@
 #
 
 set -eux
+
 make binary
 
 ./bin/armageddon generate --config="node/examples/config/example-deployment.yaml" --output="/tmp/arma-sample"  --version=2
-cd node/examples && docker-compose up -d
+cd node/examples && docker compose up -d
 sleep 10
 
-docker run --name arma-config-vol -it --mount type=bind,source=/tmp/arma-sample/config,target=/config --entrypoint /usr/local/bin/armageddon --network examples_default arma submit --config /config/party1/user_config.yaml --transactions 1000 --rate 500 --txSize 64
+docker run --name arma-config-vol -t --mount type=bind,source=/tmp/arma-sample/config,target=/config --entrypoint /usr/local/bin/armageddon --network examples_default arma submit --config /config/party1/user_config.yaml --transactions 1000 --rate 500 --txSize 64
