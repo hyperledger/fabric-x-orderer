1f3d008a50e9da55f61a0002c4f9ec0654ee8a28
Omar Agbaria <omar.agbaria@ibm.com>
2025-07-15 12:06:52 +0300

    Added comment to tx broadcast client

--- testutil/client/tx_broadcast_client.go
+++ testutil/client/tx_broadcast_client.go
@@ -82,6 +82,7 @@ func (c *BroadCastTxClient) createSendStreams() error {
 				},
 				DialTimeout: time.Second * 5,
 			}
+			// dial the router endpoint
 			gRPCRouterClientConn, err := gRPCRouterClient.Dial(userConfig.RouterEndpoints[i])
 			if err == nil {
 				stream, err := ab.NewAtomicBroadcastClient(gRPCRouterClientConn).Broadcast(context.TODO())
