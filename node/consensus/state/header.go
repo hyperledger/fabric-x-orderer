89ffe48f093c12e560e8d5365f34c83b6f1a9292
Hagar Meir <hagar.meir@ibm.com>
2025-09-03 15:32:24 +0300

    add available common blocks to header
    Signed-off-by: Hagar Meir <hagar.meir@ibm.com>

--- node/consensus/state/header.go
+++ node/consensus/state/header.go
@@ -7,25 +7,29 @@ SPDX-License-Identifier: Apache-2.0
 package state
 
 import (
+	"bytes"
 	"encoding/binary"
 
+	"github.com/hyperledger/fabric-protos-go-apiv2/common"
 	"github.com/hyperledger/fabric-x-orderer/common/types"
+	"google.golang.org/protobuf/proto"
 
 	"github.com/pkg/errors"
 )
 
 type Header struct {
-	Num             types.DecisionNum
-	AvailableBlocks []AvailableBlock
-	State           *State
+	Num                   types.DecisionNum
+	AvailableBlocks       []AvailableBlock
+	AvailableCommonBlocks []*common.Block
+	State                 *State
 }
 
 func (h *Header) Deserialize(bytes []byte) error {
 	if bytes == nil {
 		return errors.Errorf("nil bytes")
 	}
-	if len(bytes) < 8+4 { // at least header number (uint64) and length of available blocks (uint32)
-		return errors.Errorf("len of bytes is just %d; expected at least 12", len(bytes))
+	if len(bytes) < 8+4+4 { // at least header number (uint64) and number of available blocks (uint32) and number of available common blocks (uint32)
+		return errors.Errorf("len of bytes is just %d; expected at least 16", len(bytes))
 	}
 	h.Num = types.DecisionNum(binary.BigEndian.Uint64(bytes[0:8]))
 	availableBlockCount := int(binary.BigEndian.Uint32(bytes[8:12]))
@@ -42,6 +46,20 @@ func (h *Header) Deserialize(bytes []byte) error {
 		pos += availableBlockSerializedSize
 		h.AvailableBlocks = append(h.AvailableBlocks, ab)
 	}
+	availableCommonBlockCount := int(binary.BigEndian.Uint32(bytes[pos : pos+4]))
+	pos += 4
+	h.AvailableCommonBlocks = nil
+	for i := 0; i < availableCommonBlockCount; i++ {
+		rawBlockSize := int(binary.BigEndian.Uint32(bytes[pos : pos+4]))
+		pos += 4
+		rawBlock := bytes[pos : pos+rawBlockSize]
+		block := &common.Block{}
+		if err := proto.Unmarshal(rawBlock, block); err != nil {
+			return errors.Errorf("failed to unmarshal available common block; index: %d", i)
+		}
+		pos += rawBlockSize
+		h.AvailableCommonBlocks = append(h.AvailableCommonBlocks, block)
+	}
 	rawState := bytes[pos:]
 	if len(rawState) == 0 {
 		h.State = nil
@@ -55,16 +73,18 @@ func (h *Header) Deserialize(bytes []byte) error {
 
 func (h *Header) Serialize() []byte {
 	availableBlocksBytes := availableBlocksToBytes(h.AvailableBlocks)
+	availableCommonBlocksBytes := availableCommonBlocksToBytes(h.AvailableCommonBlocks)
 	rawState := []byte{}
 	if h.State != nil {
 		rawState = h.State.Serialize()
 	}
 
-	buff := make([]byte, 8+len(availableBlocksBytes)+len(rawState))
+	buff := make([]byte, 8+len(availableBlocksBytes)+len(availableCommonBlocksBytes)+len(rawState))
 
 	binary.BigEndian.PutUint64(buff, uint64(h.Num))
 	copy(buff[8:], availableBlocksBytes)
-	copy(buff[8+len(availableBlocksBytes):], rawState)
+	copy(buff[8+len(availableBlocksBytes):], availableCommonBlocksBytes)
+	copy(buff[8+len(availableBlocksBytes)+len(availableCommonBlocksBytes):], rawState)
 
 	return buff
 }
@@ -78,5 +98,24 @@ func availableBlocksToBytes(availableBlocks []AvailableBlock) []byte {
 		copy(buff[pos:], bytes)
 		pos += len(bytes)
 	}
+	return buff
+}
+
+func availableCommonBlocksToBytes(blocks []*common.Block) []byte {
+	blockBuff := bytes.Buffer{}
+	for _, b := range blocks {
+		rawBlock, err := proto.Marshal(b)
+		if err != nil {
+			panic(err)
+		}
+		rawBlockSize := make([]byte, 4)
+		binary.BigEndian.PutUint32(rawBlockSize, uint32(len(rawBlock))) // TODO check the size limit, I guess uint32 is enough
+		blockBuff.Write(rawBlockSize)
+		blockBuff.Write(rawBlock)
+	}
+	bytes := blockBuff.Bytes()
+	buff := make([]byte, 4+len(bytes))
+	binary.BigEndian.PutUint32(buff[0:4], uint32(len(blocks)))
+	copy(buff[4:], bytes)
 	return buff
 }
--- node/consensus/state/header_test.go
+++ node/consensus/state/header_test.go
@@ -10,7 +10,9 @@ import (
 	"encoding/binary"
 	"testing"
 
+	"github.com/hyperledger/fabric-protos-go-apiv2/common"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
 )
 
 func TestHeaderBytes(t *testing.T) {
@@ -33,7 +35,44 @@ func TestHeaderBytes(t *testing.T) {
 
 	require.Equal(t, hdr, hdr2)
 
-	err := hdr.Deserialize([]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7})
+	block1 := &common.Block{Header: &common.BlockHeader{Number: 1}}
+	block2 := &common.Block{Header: &common.BlockHeader{Number: 2}}
+
+	m1, err := proto.Marshal(block1)
+	require.NoError(t, err)
+	m2, err := proto.Marshal(block2)
+	require.NoError(t, err)
+
+	b1 := &common.Block{}
+	b2 := &common.Block{}
+
+	require.NoError(t, proto.Unmarshal(m1, b1))
+	require.NoError(t, proto.Unmarshal(m2, b2))
+
+	require.Equal(t, uint64(1), b1.Header.Number)
+	require.Equal(t, uint64(2), b2.Header.Number)
+
+	bm1, err := proto.Marshal(b1)
+	require.NoError(t, err)
+	bm2, err := proto.Marshal(b2)
+	require.NoError(t, err)
+
+	require.Equal(t, m1, bm1)
+	require.Equal(t, m2, bm2)
+	require.NotEqual(t, bm1, bm2)
+
+	hdr.AvailableCommonBlocks = []*common.Block{{Header: &common.BlockHeader{Number: 1}}, {Header: &common.BlockHeader{Number: 2}}}
+	require.NoError(t, hdr2.Deserialize(hdr.Serialize()))
+
+	require.Equal(t, hdr.Num, hdr2.Num)
+	require.Equal(t, hdr.State, hdr2.State)
+	require.Equal(t, hdr.AvailableBlocks, hdr2.AvailableBlocks)
+	require.Equal(t, hdr.AvailableCommonBlocks[0].Header.Number, hdr2.AvailableCommonBlocks[0].Header.Number)
+	require.Equal(t, hdr.AvailableCommonBlocks[1].Header.Number, hdr2.AvailableCommonBlocks[1].Header.Number)
+
+	require.Equal(t, hdr.Serialize(), hdr2.Serialize())
+
+	err = hdr.Deserialize([]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7})
 	require.Error(t, err)
 	t.Log(err)
 
@@ -53,11 +92,12 @@ func TestHeaderBytes(t *testing.T) {
 	t.Log(err)
 
 	// no error (nil state)
-	bytes = make([]byte, 8+4+availableBlockSerializedSize)
+	bytes = make([]byte, 8+4+availableBlockSerializedSize+4)
 	binary.BigEndian.PutUint64(bytes, 100)
 	binary.BigEndian.PutUint32(bytes[8:], 1)
 	ab := AvailableBlock{Header: &BlockHeader{10, make([]byte, 32), make([]byte, 32)}, Batch: NewAvailableBatch(3, 2, 1, make([]byte, 32))}
 	copy(bytes[12:], ab.Serialize())
+	binary.BigEndian.PutUint32(bytes[12+availableBlockSerializedSize:], 0)
 	err = hdr.Deserialize(bytes)
 	require.NoError(t, err)
 	require.Nil(t, hdr.State)
