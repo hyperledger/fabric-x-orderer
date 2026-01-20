d039e58500b913b52d9a8666495d79407b6f49ab
Hagar Meir <hagar.meir@ibm.com>
2026-01-20 16:55:57 +0200

    submit simple req
    Signed-off-by: Hagar Meir <hagar.meir@ibm.com>

--- test/consensus_test.go
+++ test/consensus_test.go
@@ -14,7 +14,9 @@ import (
 	"strconv"
 	"sync"
 	"testing"
+	"time"
 
+	"github.com/hyperledger/fabric-protos-go-apiv2/common"
 	"github.com/hyperledger/fabric-protos-go-apiv2/orderer"
 	"github.com/hyperledger/fabric-x-orderer/common/msp"
 	"github.com/hyperledger/fabric-x-orderer/common/policy"
@@ -27,6 +29,7 @@ import (
 	"github.com/hyperledger/fabric-x-orderer/node/consensus"
 	"github.com/hyperledger/fabric-x-orderer/node/consensus/state"
 	"github.com/hyperledger/fabric-x-orderer/node/crypto"
+	"github.com/hyperledger/fabric-x-orderer/node/ledger"
 	protos "github.com/hyperledger/fabric-x-orderer/node/protos/comm"
 	"github.com/hyperledger/fabric-x-orderer/testutil"
 	"github.com/hyperledger/fabric-x-orderer/testutil/tx"
@@ -152,13 +155,7 @@ func TestSubmitStopThenRestartConsenter(t *testing.T) {
 }
 
 func TestConsensusWithRealConfigUpdate(t *testing.T) {
-	// 1. Compile arma
-	armaBinaryPath, err := gexec.BuildWithEnvironment("github.com/hyperledger/fabric-x-orderer/cmd/arma", []string{"GOPRIVATE=" + os.Getenv("GOPRIVATE")})
-	defer gexec.CleanupBuildArtifacts()
-	require.NoError(t, err)
-	require.NotNil(t, armaBinaryPath)
-
-	// 2. Create a temporary directory for the test.
+	// Create a temporary directory for the test.
 	dir, err := os.MkdirTemp("", t.Name())
 	require.NoError(t, err)
 	defer os.RemoveAll(dir)
@@ -166,61 +163,73 @@ func TestConsensusWithRealConfigUpdate(t *testing.T) {
 	numOfParties := 1
 	numOfShards := 1
 
-	// 3. Create a config YAML file in the temporary directory.
+	// Create a config YAML file in the temporary directory.
 	configPath := filepath.Join(dir, "config.yaml")
 	netInfo := testutil.CreateNetwork(t, configPath, numOfParties, numOfShards, "TLS", "none")
-	require.NoError(t, err)
+	require.NotNil(t, netInfo)
 
-	// 4. Generate the config files in the temporary directory using the armageddon generate command.
+	// Generate the config files in the temporary directory using the armageddon generate command.
 	armageddon.NewCLI().Run([]string{"generate", "--config", configPath, "--output", dir})
 
-	configStoreDir := t.TempDir()
-	defer os.RemoveAll(configStoreDir)
-
-	netInfo["Party11consensus"].Listener.Close()
+	// Update the file store path
+	fileStoreDir := t.TempDir()
+	defer os.RemoveAll(fileStoreDir)
 	nodeConfigPath := filepath.Join(dir, "config", "party1", "local_config_consenter.yaml")
-
 	localConfig, _, err := config.LoadLocalConfig(nodeConfigPath)
 	require.NoError(t, err)
-
-	localConfig.NodeLocalConfig.FileStore.Path = configStoreDir
+	localConfig.NodeLocalConfig.FileStore.Path = fileStoreDir
 	utils.WriteToYAML(localConfig.NodeLocalConfig, nodeConfigPath)
 
+	// Get all for create consensus
 	configContent, lastConfigBlock, err := config.ReadConfig(nodeConfigPath, testutil.CreateLoggerForModule(t, "ReadConfigConsensus", zap.DebugLevel))
 	require.NoError(t, err)
-
 	consenterConfig := configContent.ExtractConsenterConfig(lastConfigBlock)
 	require.NotNil(t, consenterConfig)
-
 	localmsp := msp.BuildLocalMSP(configContent.LocalConfig.NodeLocalConfig.GeneralConfig.LocalMSPDir, configContent.LocalConfig.NodeLocalConfig.GeneralConfig.LocalMSPID, configContent.LocalConfig.NodeLocalConfig.GeneralConfig.BCCSP)
 	signer, err := localmsp.GetDefaultSigningIdentity()
 	require.NoError(t, err)
-
 	consenterLogger := testutil.CreateLogger(t, int(numOfParties))
-
 	server := arma_node.CreateGRPCConsensus(consenterConfig)
 	consensus := consensus.CreateConsensus(consenterConfig, server, lastConfigBlock, consenterLogger, signer, &policy.DefaultConfigUpdateProposer{})
 
+	// Register and start grpc server
 	protos.RegisterConsensusServer(server.Server(), consensus)
 	orderer.RegisterAtomicBroadcastServer(server.Server(), consensus.DeliverService)
 	orderer.RegisterClusterNodeServiceServer(server.Server(), consensus)
-
 	go func() {
 		server.Start()
 	}()
 
+	// Start consensus
 	consensus.Start()
+	ledgerListener := &storageListener{c: make(chan *common.Block, 100)}
+	consensus.Storage.(*ledger.ConsensusLedger).RegisterAppendListener(ledgerListener)
 
+	// Submit to consensus a simple request (baf) from batcher
 	digest := make([]byte, 32-3)
 	digest123 := append([]byte{1, 2, 3}, digest...)
-
-	keyBytes, err := os.ReadFile(filepath.Join(dir, "crypto/ordererOrganizations/org1/orderers/party1/batcher1/tls/key.pem"))
+	keyBytes, err := os.ReadFile(filepath.Join(dir, "crypto/ordererOrganizations/org1/orderers/party1/batcher1/msp/keystore/priv_sk"))
 	require.NoError(t, err)
 	privateKey, err := tx.CreateECDSAPrivateKey(keyBytes)
 	require.NoError(t, err, "failed to create private key")
-
 	baf, err := batcher_node.CreateBAF((*crypto.ECDSASigner)(privateKey), 1, 1, digest123, 1, 0, 0)
 	require.NoError(t, err)
 	controlEvent := &state.ControlEvent{BAF: baf}
 	consensus.SubmitRequest(controlEvent.Bytes())
+	require.Eventually(t, func() bool {
+		b := <-ledgerListener.c
+		return b.Header.Number == uint64(1)
+	}, 30*time.Second, 100*time.Millisecond)
+}
+
+type storageListener struct {
+	c chan *common.Block
+	f func()
+}
+
+func (l *storageListener) OnAppend(block *common.Block) {
+	if l.f != nil {
+		defer l.f()
+	}
+	l.c <- block
 }
