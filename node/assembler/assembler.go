f18efbb93680142b86738ff701f92b306082e93c
Genady Gurevich <genadyg@il.ibm.com>
2026-01-21 17:16:19 +0200

    Operations endpoints: logspec handler (issue #45)
    Signed-off-by: Genady Gurevich <genadyg@il.ibm.com>

--- node/assembler/assembler.go
+++ node/assembler/assembler.go
@@ -162,6 +162,7 @@ func NewDefaultAssembler(
 
 	assembler.logger.Infof("Prometheus serving on URL: %s", operations.PrometheusMetricsServiceURL(assembler.opsSystem, assembler.logger))
 	assembler.logger.Infof("Health check serving on URL: %s", operations.HealthCheckServiceURL(assembler.opsSystem, assembler.logger))
+	assembler.logger.Infof("Logging spec service serving on URL: %s", operations.LogSpecServiceURL(assembler.opsSystem, assembler.logger))
 	assembler.metrics.StartMetricsTracker()
 
 	return assembler
