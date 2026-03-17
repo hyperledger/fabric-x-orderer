efff194caa8d83f465f699a65141c7651b14b929
May.Buzaglo <May.Buzaglo@ibm.com>
2026-03-17 18:28:11 +0200

    address review comments
    Signed-off-by: May.Buzaglo <May.Buzaglo@ibm.com>

--- node/batcher/batcher_reconfig_test.go
+++ node/batcher/batcher_reconfig_test.go
@@ -14,14 +14,12 @@ import (
 
 	"github.com/hyperledger/fabric-protos-go-apiv2/common"
 	"github.com/hyperledger/fabric-x-common/common/channelconfig"
-	"github.com/hyperledger/fabric-x-common/protoutil"
 	"github.com/hyperledger/fabric-x-orderer/common/tools/armageddon"
 	"github.com/hyperledger/fabric-x-orderer/common/types"
 	"github.com/hyperledger/fabric-x-orderer/common/utils"
 	"github.com/hyperledger/fabric-x-orderer/config"
 	"github.com/hyperledger/fabric-x-orderer/config/protos"
 	"github.com/hyperledger/fabric-x-orderer/node/batcher"
-	"github.com/hyperledger/fabric-x-orderer/node/consensus"
 	"github.com/hyperledger/fabric-x-orderer/node/consensus/state"
 	node_utils "github.com/hyperledger/fabric-x-orderer/node/utils"
 	"github.com/hyperledger/fabric-x-orderer/testutil"
@@ -45,6 +43,12 @@ func TestBatcherReceivesConfigBlockFromConsensusAndApplyConfig_ChangeBatchTimeou
 	configPath := filepath.Join(dir, "config.yaml")
 	netInfo := testutil.CreateNetwork(t, configPath, len(parties), numOfShards, "TLS", "none")
 	require.NotNil(t, netInfo)
+
+	for _, n := range netInfo {
+		if n.Listener != nil {
+			_ = n.Listener.Close()
+		}
+	}
 
 	armageddon.NewCLI().Run([]string{"generate", "--config", configPath, "--output", dir})
 
@@ -75,16 +79,7 @@ func TestBatcherReceivesConfigBlockFromConsensusAndApplyConfig_ChangeBatchTimeou
 	configUpdatePbData := configUpdateBuilder.UpdateBatchTimeouts(t, cfgutil.NewBatchTimeoutsConfig(cfgutil.BatchTimeoutsConfigName.AutoRemoveTimeout, "15ms"))
 	require.NotNil(t, configUpdatePbData)
 	configUpdateEnvelope := cfgutil.CreateConfigTX(t, dir, parties, 1, configUpdatePbData)
-	configEnvelope, err := bundle.ConfigtxValidator().ProposeConfigUpdate(configUpdateEnvelope)
-	require.NoError(t, err)
-	env, err := protoutil.CreateSignedEnvelope(common.HeaderType_CONFIG, bundle.ConfigtxValidator().ChannelID(), nil, configEnvelope, int32(0), 0)
-	require.NoError(t, err)
-	require.NotNil(t, env)
-	configReq, err := protoutil.Marshal(env)
-	require.NoError(t, err)
-	require.NotNil(t, configReq)
-	prevHash := protoutil.BlockHeaderHash(genesisBlock.Header)
-	configBlock, err := consensus.CreateConfigCommonBlock(genesisBlock.GetHeader().GetNumber()+1, prevHash, 1, types.DecisionNum(1), 1, 0, configReq)
+	configBlock, err := cfgutil.CreateConsensusConfigBlock(bundle, configUpdateEnvelope, genesisBlock.Header, 1, types.DecisionNum(1), 1, 0)
 	require.NoError(t, err)
 	availableCommonBlocks := []*common.Block{configBlock}
 	shardID := types.ShardID(1)
@@ -94,7 +89,7 @@ func TestBatcherReceivesConfigBlockFromConsensusAndApplyConfig_ChangeBatchTimeou
 		stubConsenters[i].UpdateStateHeaderWithConfigBlock(types.DecisionNum(1), availableCommonBlocks, state)
 	}
 
-	// batchers append the config tx to the config store
+	// wait for batchers to append the config tx to the config store
 	for j := range parties {
 		require.Eventually(t, func() bool {
 			block, err1 := batchers[j].ConfigStore.Last()
@@ -120,15 +115,15 @@ func createBatcherNodes(t *testing.T, dir string, parties []types.PartyID, numOf
 	for i, partyID := range parties {
 		for j := 1; j <= numOfShards; j++ {
 			nodeConfigPath := filepath.Join(dir, "config", fmt.Sprintf("party%d", partyID), fmt.Sprintf("local_config_batcher%d.yaml", j))
-			config, lastConfigBlock, err := config.ReadConfig(nodeConfigPath, testutil.CreateLoggerForModule(t, fmt.Sprintf("ReadConfigBatcher%d%d", partyID, j), zap.DebugLevel))
+			nodeConfig, lastConfigBlock, err := config.ReadConfig(nodeConfigPath, testutil.CreateLoggerForModule(t, fmt.Sprintf("ReadConfigBatcher%d%d", partyID, j), zap.DebugLevel))
 			require.NoError(t, err)
-			batcherConfig := config.ExtractBatcherConfig(lastConfigBlock)
+			batcherConfig := nodeConfig.ExtractBatcherConfig(lastConfigBlock)
 			require.NotNil(t, batcherConfig)
-			_, signer, err := testutil.BuildTestLocalMSP(config.LocalConfig.NodeLocalConfig.GeneralConfig.LocalMSPDir, config.LocalConfig.NodeLocalConfig.GeneralConfig.LocalMSPID)
+			_, signer, err := testutil.BuildTestLocalMSP(nodeConfig.LocalConfig.NodeLocalConfig.GeneralConfig.LocalMSPDir, nodeConfig.LocalConfig.NodeLocalConfig.GeneralConfig.LocalMSPID)
 			require.NoError(t, err)
 			require.NotNil(t, signer)
 			batcherLogger := testutil.CreateLogger(t, int(partyID))
-			batcher := batcher.CreateBatcher(batcherConfig, config, batcherLogger, make(chan struct{}), consenters[i], &batcher.ConsenterControlEventSenderFactory{}, signer)
+			batcher := batcher.CreateBatcher(batcherConfig, nodeConfig, batcherLogger, make(chan struct{}), consenters[i], &batcher.ConsenterControlEventSenderFactory{}, signer)
 			batcherNodes = append(batcherNodes, batcher)
 			genesisBlock = lastConfigBlock
 			bundle = batcherConfig.Bundle
@@ -148,17 +143,17 @@ func createStubConsenters(t *testing.T, dir string, parties []types.PartyID) []*
 	consenterNodes := make([]*stubConsenter, 0, len(parties))
 	for _, i := range parties {
 		nodeConfigPath := filepath.Join(dir, "config", fmt.Sprintf("party%d", i), "local_config_consenter.yaml")
-		config, lastConfigBlock, err := config.ReadConfig(nodeConfigPath, testutil.CreateLoggerForModule(t, fmt.Sprintf("ReadConfigConsenter%d", i), zap.DebugLevel))
+		nodeConfig, lastConfigBlock, err := config.ReadConfig(nodeConfigPath, testutil.CreateLoggerForModule(t, fmt.Sprintf("ReadConfigConsenter%d", i), zap.DebugLevel))
 		require.NoError(t, err)
 		var partyConfig *protos.PartyConfig
-		for _, p := range config.SharedConfig.PartiesConfig {
+		for _, p := range nodeConfig.SharedConfig.PartiesConfig {
 			if types.PartyID(p.PartyID) == i {
 				partyConfig = p
 				break
 			}
 		}
 		require.NotNil(t, partyConfig)
-		consenterConfig := config.ExtractConsenterConfig(lastConfigBlock)
+		consenterConfig := nodeConfig.ExtractConsenterConfig(lastConfigBlock)
 		require.NotNil(t, consenterConfig)
 		srv := node_utils.CreateGRPCConsensus(consenterConfig)
 		require.NotNil(t, srv)
---
+++ testutil/configutil/consensus_config_block_utils.go
@@ -0,0 +1,40 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package configutil
+
+import (
+	"github.com/hyperledger/fabric-protos-go-apiv2/common"
+	"github.com/hyperledger/fabric-x-common/common/channelconfig"
+	"github.com/hyperledger/fabric-x-orderer/common/types"
+	"github.com/hyperledger/fabric-x-orderer/node/consensus"
+	"github.com/hyperledger/fabric/protoutil"
+)
+
+// CreateConsensusConfigBlock created a config block received from consensus.
+func CreateConsensusConfigBlock(bundle channelconfig.Resources, configUpdateEnvelope *common.Envelope, prevBlockHeader *common.BlockHeader, txCount uint64, decisionNum types.DecisionNum, batchCount int, batchIndex int) (*common.Block, error) {
+	configEnvelope, err := bundle.ConfigtxValidator().ProposeConfigUpdate(configUpdateEnvelope)
+	if err != nil {
+		return nil, err
+	}
+	env, err := protoutil.CreateSignedEnvelope(common.HeaderType_CONFIG, bundle.ConfigtxValidator().ChannelID(), nil, configEnvelope, int32(0), 0)
+	if err != nil {
+		return nil, err
+	}
+
+	configReq, err := protoutil.Marshal(env)
+	if err != nil {
+		return nil, err
+	}
+
+	prevHash := protoutil.BlockHeaderHash(prevBlockHeader)
+	configBlock, err := consensus.CreateConfigCommonBlock(prevBlockHeader.GetNumber()+1, prevHash, txCount, decisionNum, batchCount, batchIndex, configReq)
+	if err != nil {
+		return nil, err
+	}
+
+	return configBlock, nil
+}
