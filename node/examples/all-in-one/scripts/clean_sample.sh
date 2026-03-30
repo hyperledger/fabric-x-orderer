25e1333d97746192824b4fd9db5448b1da7d3fd7
Moran Abilea <mor2451@gmail.com>
2026-03-30 10:00:41 +0300

    All in one final review notes (#740)
    * fixed readme tiny notes
    
    Signed-off-by: Moran Abilea <moran.abilea@ibm.com>
    
    * fixe more readme notes
    
    Signed-off-by: Moran Abilea <moran.abilea@ibm.com>
    
    * fix run_sample review notes
    
    Signed-off-by: Moran Abilea <moran.abilea@ibm.com>
    
    * fix clear review notes
    
    Signed-off-by: Moran Abilea <moran.abilea@ibm.com>
    
    * extra fix for non-root user
    
    Signed-off-by: Moran Abilea <moran.abilea@ibm.com>
    
    ---------
    
    Signed-off-by: Moran Abilea <moran.abilea@ibm.com>

--- node/examples/all-in-one/scripts/clean_sample.sh
+++ node/examples/all-in-one/scripts/clean_sample.sh
@@ -7,6 +7,10 @@
 
 set -eux
 
+BASE=/tmp/arma-all-in-one
+HOST_UID=$(id -u)
+HOST_GID=$(id -g)
+
 echo "Stopping all-in-one container..."
 
 docker ps -q --filter "ancestor=arma-4p1s" | xargs -r docker stop
@@ -15,8 +19,18 @@ echo "Removing container..."
 
 docker ps -aq --filter "ancestor=arma-4p1s" | xargs -r docker rm
 
+echo "Fixing ownership and permissions..."
+
+if [ -d "${BASE}" ]; then
+  docker run --rm \
+    -u 0:0 \
+    -v "${BASE}:${BASE}" \
+    alpine \
+    sh -c "chown -R ${HOST_UID}:${HOST_GID} '${BASE}' && chmod -R u+rwX '${BASE}'"
+fi
+
 echo "Removing storage..."
 
-rm -rf /tmp/arma-all-in-one
+rm -rf "${BASE}"
 
 echo "Clean done."
