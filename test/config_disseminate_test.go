e1eff9e2426ef300e24715c1298515f7810cca58
May.Buzaglo <May.Buzaglo@ibm.com>
2026-04-14 12:13:57 +0300

    update config tx in TestConfigTXDisseminationWithVerification
    Signed-off-by: May.Buzaglo <May.Buzaglo@ibm.com>

--- test/config_disseminate_test.go
+++ test/config_disseminate_test.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -218,6 +219,10 @@ func TestConfigDisseminate(t *testing.T) {
 	}
 }
 
+// TestConfigTXDisseminationWithVerification verifies that a config tx modifying a SmartBFT parameter is properly disseminated across all nodes.
+// All nodes are expected to receive the tx and append it to the respective config store (routers and batchers) and ledgers (consenters and assebmlers),
+// The network then undergoes a dynamic reconfiguration process after which it becomes available again.
+// Once the config tx is applied, improperly signed data txs should be rejected, while properly signed ones should be processed successfully.
 func TestConfigTXDisseminationWithVerification(t *testing.T) {
 	// Compile Arma
 	armaBinaryPath, err := gexec.BuildWithEnvironment("github.com/hyperledger/fabric-x-orderer/cmd/arma", []string{"GOPRIVATE=" + os.Getenv("GOPRIVATE")})
@@ -264,7 +269,8 @@ func TestConfigTXDisseminationWithVerification(t *testing.T) {
 	configUpdateBuilder, cleanUp := cfgutil.NewConfigUpdateBuilder(t, dir, genesisBlockPath)
 	defer cleanUp()
 
-	configUpdatePbData := configUpdateBuilder.UpdateBatchSizeConfig(t, cfgutil.NewBatchSizeConfig(cfgutil.BatchSizeConfigName.MaxMessageCount, 500))
+	requestBatchMaxBytes := uint64(1048576)
+	configUpdatePbData := configUpdateBuilder.UpdateSmartBFTConfig(t, cfgutil.NewSmartBFTConfig(cfgutil.SmartBFTConfigName.RequestBatchMaxBytes, strconv.FormatUint(requestBatchMaxBytes, 10)))
 	require.NotEmpty(t, configUpdatePbData)
 
 	env := cfgutil.CreateConfigTX(t, dir, []types.PartyID{1, 2, 3, 4}, submittingParty, configUpdatePbData)
