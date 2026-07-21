d05d8866cae7cfd9856cc7bef64eec59930da014
Yoav Tock <tock@il.ibm.com>
2026-07-21 15:37:18 +0300

    update .gitignore (#1027)
    Signed-off-by: Yoav Tock <TOCK@il.ibm.com>

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -14,6 +14,7 @@ function filterExcludedAndGeneratedFiles {
         '^build/'
         '(^|/)ci\.properties$'
         '(^|/)\.git/'
+        '(^|/)\.gitignore$'
         '\.gen\.go$'
         '(^|/)go.mod$'
         '(^|/)go.sum$'
