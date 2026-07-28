fe2d3f3fe452127eb00d3a880c1b0cb4ebbb4fba
HagarMeir <hagar.meir@ibm.com>
2026-07-28 15:26:42 +0300

    move config req out of state (#1051)
    Signed-off-by: Hagar Meir <hagar.meir@ibm.com>

--- node/consensus/state/state.go
+++ node/consensus/state/state.go
@@ -17,13 +17,9 @@ import (
 	"strings"
 
 	"github.com/hyperledger/fabric-lib-go/common/flogging"
-	"github.com/hyperledger/fabric-protos-go-apiv2/common"
-	"github.com/hyperledger/fabric-x-common/common/configtx"
-	"github.com/hyperledger/fabric-x-common/protoutil"
 	"github.com/hyperledger/fabric-x-orderer/common/types"
 	"github.com/hyperledger/fabric-x-orderer/common/utils"
 	stateprotos "github.com/hyperledger/fabric-x-orderer/node/protos/state"
-	"github.com/pkg/errors"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -207,72 +203,6 @@ func (s *State) Deserialize(rawBytes []byte) error {
 type ShardTerm struct {
 	Shard types.ShardID
 	Term  uint64
-}
-
-type ConfigRequest struct {
-	Envelope *common.Envelope
-}
-
-func (c *ConfigRequest) ConfigSequence() (types.ConfigSequence, error) {
-	payload, err := protoutil.UnmarshalPayload(c.Envelope.Payload)
-	if err != nil {
-		return 0, errors.Wrap(err, "failed to unmarshal payload")
-	}
-
-	configEnvelope, err := configtx.UnmarshalConfigEnvelope(payload.Data)
-	if err != nil {
-		return 0, errors.Wrap(err, "failed to unmarshal config envelope")
-	}
-
-	if configEnvelope.Config == nil {
-		return 0, errors.New("config envelope has nil config")
-	}
-
-	return types.ConfigSequence(configEnvelope.Config.Sequence), nil
-}
-
-func (c *ConfigRequest) toProto() *stateprotos.ConfigRequest {
-	envelopeBytes, err := protoutil.Marshal(c.Envelope)
-	if err != nil {
-		panic(fmt.Sprintf("failed to marshal envelope: %v", err))
-	}
-	return &stateprotos.ConfigRequest{
-		Envelope: envelopeBytes,
-	}
-}
-
-func (c *ConfigRequest) fromProto(pc *stateprotos.ConfigRequest) error {
-	envelope, err := protoutil.UnmarshalEnvelope(pc.Envelope)
-	if err != nil {
-		return fmt.Errorf("failed to unmarshal envelope: %v", err)
-	}
-	c.Envelope = envelope
-	return nil
-}
-
-func (c *ConfigRequest) Bytes() []byte {
-	protoConfigRequest := c.toProto()
-	bytes, err := proto.Marshal(protoConfigRequest)
-	if err != nil {
-		panic(fmt.Sprintf("failed to marshal config request: %v", err))
-	}
-	return bytes
-}
-
-func (c *ConfigRequest) FromBytes(bytes []byte) error {
-	protoConfigRequest := &stateprotos.ConfigRequest{}
-	if err := proto.Unmarshal(bytes, protoConfigRequest); err != nil {
-		return fmt.Errorf("failed to unmarshal config request: %v", err)
-	}
-	return c.fromProto(protoConfigRequest)
-}
-
-func (c *ConfigRequest) String() string {
-	configSeq, err := c.ConfigSequence()
-	if err != nil {
-		return fmt.Sprintf("Config Request with error: %v", err)
-	}
-	return fmt.Sprintf("Config Request with config sequence %d", configSeq)
 }
 
 type ControlEvent struct {
